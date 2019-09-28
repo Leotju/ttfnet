6427d59aa1cb4945384fcd4620b8fae38448e30d
Leotju <xj65112338>
2019-09-28 22:57:19 +0800

    0925

---
+++ cfgs_fatdet/pascal/baseline/ttfnet_r18_3x_lr016_no_pretrain_gn.py
@@ -0,0 +1,121 @@
+# model settings
+model = dict(
+    type='TTFNet',
+    pretrained=None,
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=-1,
+        norm_eval=False,
+        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
+        style='pytorch'),
+    neck=None,
+    bbox_head=dict(
+        type='TTFHead',
+        inplanes=(64, 128, 256, 512),
+        head_conv=128,
+        wh_conv=64,
+        hm_head_conv_num=2,
+        wh_head_conv_num=1,
+        num_classes=21,
+        wh_offset_base=16,
+        wh_agnostic=True,
+        wh_gaussian=True,
+        shortcut_cfg=(1, 2, 3),
+        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
+        alpha=0.54,
+        hm_weight=1.,
+        wh_weight=5.))
+cudnn_benchmark = True
+# training and testing settings
+train_cfg = dict(
+    vis_every_n_iters=100,
+    debug=False)
+test_cfg = dict(
+    score_thr=0.01,
+    max_per_img=100)
+# dataset settings
+dataset_type = 'VOCDataset'
+data_root = '../data/VOCdevkit/'
+# data_root = '/media/leo/data/datasets/VOC/VOCdevkit/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data = dict(
+    imgs_per_gpu=16,
+    workers_per_gpu=2,
+    train=dict(
+        type='RepeatDataset',  # to avoid reloading datasets frequently
+        times=9,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=[
+                data_root + 'VOC2007/ImageSets/Main/trainval.txt',
+                data_root + 'VOC2012/ImageSets/Main/trainval.txt'
+            ],
+            img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
+            img_scale=(512, 512),
+            img_norm_cfg=img_norm_cfg,
+            size_divisor=32,
+            flip_ratio=0.5,
+            with_mask=False,
+            with_crowd=False,
+            with_label=True,
+            resize_keep_ratio=False)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        img_scale=(512, 512),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=True,
+        resize_keep_ratio=False),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        img_scale=(512, 512),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=False,
+        test_mode=True,
+        resize_keep_ratio=False))
+# optimizer
+optimizer = dict(type='SGD', lr=0.016, momentum=0.9, weight_decay=0.0004,
+                 paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 5,
+    step=[3])
+checkpoint_config = dict(interval=1)
+bbox_head_hist_config = dict(
+    model_type=['ConvModule', 'DeformConvPack'],
+    sub_modules=['bbox_head'],
+    save_every_n_steps=500)
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 4
+device_ids = range(8)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = '../work_dirs/pascal/baseline/ttfnet18_3x_lr016_no_pre_train_gn'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
--- vpa.sh
+++ vpa.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r101_1x_lr004.py 2
-./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r18_1x_lr004_ep12.py 2
-./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r18_1x_lr008.py 2
+./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r18_3x_lr16_no_pretrain_bn.py 2
+./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r18_3x_lr016_no_pretrain_bn.py 2
+./tools/dist_train.sh cfgs_fatdet/pascal/baseline/ttfnet_r18_3x_lr016_no_pretrain_gn.py 2
