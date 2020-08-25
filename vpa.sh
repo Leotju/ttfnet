e3a88a9ae3ff9a15ed80987c0bc743e2fb2b9978
Leotju <xj65112338>
2020-08-25 22:37:04 +0800

    0925

--- vpa.sh
+++ vpa.sh
@@ -1,2 +1,3 @@
+./tools/dist_train.sh cfgs_fatdet/voc/baseline/ttfnet_r18_384_10x_aug_no_pretrain.py 2
 ./tools/dist_train.sh cfgs_fatdet/voc/baseline/ttfnet_r18_384_1x.py 2
 ./tools/dist_train.sh cfgs_fatdet/voc/baseline/ttfnet_r18_384_1x_no_pretrain.py 2
