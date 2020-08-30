523e98b1a41d16ea38e9b7c9677d5b8fe659d031
Leotju <xj65112338>
2020-08-30 21:12:47 +0800

    0925

---
+++ cfgs_fatdet/voc/fatnat/ttfnet_fatnet_ttfheadfull_96_10x_aug_no_pretrain.py
@@ -0,0 +1,129 @@
+# model settings
+model = dict(
+    type='TTFNet',
+    # pretrained='modelzoo://resnet18',
+    pretrained=None,
+    backbone=dict(
+        type='FatNetSimple',
+        norm_cfg = dict(type='BN', requires_grad=True),
+),
+    neck=None,
+    bbox_head=dict(
+        type='TTFHeadFull',
+        inplanes=16,
+        planes=64,
+        head_conv=64,
+        down_ratio=1,
+        wh_conv=64,
+        hm_head_conv_num=2,
+        wh_head_conv_num=1,
+        num_classes=21,
+        wh_agnostic=True,
+        wh_gaussian=True,
+        norm_cfg=dict(type='BN'),
+        alpha=0.54,
+        hm_weight=1.,
+        wh_weight=5.))
+cudnn_benchmark = True
+# training and testing settings
+train_cfg = dict(
+    vis_every_n_iters=100,
+    debug=False)
+test_cfg = dict(
+    score_thr=0.01,
+    max_per_img=100)
+# dataset settings
+dataset_type = 'VOCDataset'
+data_root = '../data/VOCdevkit/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data = dict(
+    imgs_per_gpu=16,
+    workers_per_gpu=2,
+    train=dict(
+        type='RepeatDataset',  # to avoid reloading datasets frequently
+        times=30,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=[
+                data_root + 'VOC2007/ImageSets/Main/trainval.txt',
+                data_root + 'VOC2012/ImageSets/Main/trainval.txt'
+            ],
+            img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
+            img_scale=(96, 96),
+            img_norm_cfg=img_norm_cfg,
+            size_divisor=32,
+            flip_ratio=0.5,
+            with_mask=False,
+            with_crowd=False,
+            with_label=True,
+            extra_aug=dict(
+                photo_metric_distortion=dict(
+                    brightness_delta=32,
+                    contrast_range=(0.5, 1.5),
+                    saturation_range=(0.5, 1.5),
+                    hue_delta=18),
+                expand=dict(
+                    mean=img_norm_cfg['mean'],
+                    to_rgb=img_norm_cfg['to_rgb'],
+                    ratio_range=(1, 4)),
+                random_crop=dict(
+                    min_ious=(0.1, 0.3, 0.5, 0.7, 0.9), min_crop_size=0.3)),
+            resize_keep_ratio=False)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        img_scale=(512, 512),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=True,
+        resize_keep_ratio=False),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        img_scale=(96, 96),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=False,
+        test_mode=True,
+        resize_keep_ratio=False))
+# optimizer
+optimizer = dict(type='SGD', lr=0.016, momentum=0.9, weight_decay=0.0004,
+                 paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 5,
+    step=[3])
+checkpoint_config = dict(interval=1)
+bbox_head_hist_config = dict(
+    model_type=['ConvModule', 'DeformConvPack'],
+    sub_modules=['bbox_head'],
+    save_every_n_steps=500)
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 4
+device_ids = range(8)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = '../work_dirs/pascal/baseline/ttfnet_fatnet_ttfhead_full_96_10x_aug_no_pretrain'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
