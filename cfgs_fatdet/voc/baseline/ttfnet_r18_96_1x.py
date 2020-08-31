94eea28ea22215310140caee492d5de2b01b3d04
Leotju <xj65112338>
2020-08-31 08:34:53 +0800

    0925

--- cfgs_fatdet/voc/baseline/ttfnet_r18_96_1x.py
+++ cfgs_fatdet/voc/baseline/ttfnet_r18_96_1x.py
@@ -1,25 +1,28 @@
 # model settings
 model = dict(
     type='TTFNet',
-    # pretrained='modelzoo://resnet18',
-    pretrained=None,
+    pretrained='modelzoo://resnet18',
     backbone=dict(
-        type='FatNetSimple',
-        norm_cfg = dict(type='BN', requires_grad=True),
-),
+        type='ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_eval=False,
+        style='pytorch'),
     neck=None,
     bbox_head=dict(
-        type='TTFHeadFull',
-        inplanes=16,
-        planes=64,
-        head_conv=64,
-        down_ratio=1,
+        type='TTFHead',
+        inplanes=(64, 128, 256, 512),
+        head_conv=128,
         wh_conv=64,
         hm_head_conv_num=2,
         wh_head_conv_num=1,
         num_classes=21,
+        wh_offset_base=16,
         wh_agnostic=True,
         wh_gaussian=True,
+        shortcut_cfg=(1, 2, 3),
         norm_cfg=dict(type='BN'),
         alpha=0.54,
         hm_weight=1.,
@@ -42,7 +45,7 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',  # to avoid reloading datasets frequently
-        times=30,
+        times=3,
         dataset=dict(
             type=dataset_type,
             ann_file=[
@@ -57,18 +60,6 @@ data = dict(
             with_mask=False,
             with_crowd=False,
             with_label=True,
-            extra_aug=dict(
-                photo_metric_distortion=dict(
-                    brightness_delta=32,
-                    contrast_range=(0.5, 1.5),
-                    saturation_range=(0.5, 1.5),
-                    hue_delta=18),
-                expand=dict(
-                    mean=img_norm_cfg['mean'],
-                    to_rgb=img_norm_cfg['to_rgb'],
-                    ratio_range=(1, 4)),
-                random_crop=dict(
-                    min_ious=(0.1, 0.3, 0.5, 0.7, 0.9), min_crop_size=0.3)),
             resize_keep_ratio=False)),
     val=dict(
         type=dataset_type,
@@ -122,8 +113,7 @@ total_epochs = 4
 device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = '../work_dirs/pascal/baseline/ttfnet_fatnet_ttfhead_full_96_10x_aug_no_pretrain'
+work_dir = '../work_dirs/pascal/baseline/ttfnet_r18_96_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-# 26.6
